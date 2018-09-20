baa12de50c6e0296b1fa59c40124134ea9eb399b
Cherian Porter <cherianbelun@gmail.com>
2018-09-20 00:48:40 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,29 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let userCode=[];
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+ 
+ function keySequence (e){
+  userCode.push(e.which)
+  
+  for(let i=0; i<=code.length-1;){
+     if (userCode[i] === code[i])
+    
+  }
+ 
+ 
+ 
+ 
+ 
+   loop through code
+   if code matches userCode
+   the alert('yes its correct')
+   else 
+   userCode= []
+   userCode.push(e.which);
+   
+   alert('the key pressed is ' + e.which)
+ }
+ document.addEventListener('keydown', keySequence)
+ 
 }
